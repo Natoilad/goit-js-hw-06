30fc32a169c4b3669e674684c0f0e6570e02769b
Natoilad <109770565+Natoilad@users.noreply.github.com>
2022-12-04 20:59:42 +0200

    Update task-02.js

--- js/task-02.js
+++ js/task-02.js
@@ -1,8 +1,18 @@
 const ingredients = [
-  'Potatoes',
-  'Mushrooms',
-  'Garlic',
-  'Tomatos',
-  'Herbs',
-  'Condiments',
+  "Potatoes",
+  "Mushrooms",
+  "Garlic",
+  "Tomatos",
+  "Herbs",
+  "Condiments",
 ];
+
+const ingredientsList = [];
+ingredients.forEach((el) => {
+  const li = document.createElement("li");
+  li.classList.add("item");
+  li.textContent = `${el}`;
+  ingredientsList.push(li);
+});
+const ingredientsUl = document.getElementById("ingredients");
+ingredientsUl.append(...ingredientsList);
