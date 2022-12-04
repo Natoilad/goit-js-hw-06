4495d2ac752733e1107b8c5d0e5aa4cc497b79dc
Natoilad <109770565+Natoilad@users.noreply.github.com>
2022-12-05 01:12:58 +0200

    Update task-08.js

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,20 @@
+const inputEm = document.querySelector("email");
+const inputPass = document.querySelector("password");
 
+const form = document.querySelector(".login-form");
+form.addEventListener("submit", login);
+function login(event) {
+  event.preventDefault();
+  const email = event.currentTarget.elements.email.value;
+  const password = event.currentTarget.elements.password.value;
+
+  if (!email || !password) {
+    return alert("всі поля повинні бути заповнені.");
+  }
+  const dataForm = {
+    email,
+    password,
+  };
+  console.log(dataForm);
+  form.reset();
+}
