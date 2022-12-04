06152f9dba1187d711f8e59c7a39766199492331
Natoilad <109770565+Natoilad@users.noreply.github.com>
2022-12-05 01:30:09 +0200

    Update task-09.js

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,13 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+const btnEl = document.querySelector(".change-color");
+console.dir(btnEl);
+btnEl.addEventListener("click", changeColor);
+
+function changeColor() {
+  const hexColor = getRandomHexColor();
+  const bodyEl = document.querySelector("body");
+  bodyEl.style.backgroundColor = `${hexColor}`;
+  document.querySelector(".color").textContent = hexColor;
+}
